24aae3445df7515656009a9b6f2eac86419f49d0
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-01-19 12:48:27 +0900

    Update app.py

--- ReadAloud/app.py
+++ ReadAloud/app.py
@@ -1,3 +1,7 @@
+import pyopenjtalk
+import pyworld as pw
+import numpy as np
+import soundfile as sf
 import streamlit as st
 from gtts import gTTS
 import tempfile
@@ -43,3 +47,23 @@ if st.button("音声生成"):
             )
 
         os.remove(audio_path)
+
+text = "イントネーションを調整します"
+
+# 音声生成（素の音声）
+x, sr = pyopenjtalk.tts(text)
+
+# WORLDで解析
+_f0, t = pw.dio(x.astype(np.float64), sr)
+f0 = pw.stonemask(x.astype(np.float64), _f0, t, sr)
+sp = pw.cheaptrick(x.astype(np.float64), f0, t, sr)
+ap = pw.d4c(x.astype(np.float64), f0, t, sr)
+
+# アクセント操作（例：全体を少し高く）
+f0 *= 1.2
+
+# 再合成
+y = pw.synthesize(f0, sp, ap, sr)
+
+# 保存
+sf.write("output.wav", y, sr)
