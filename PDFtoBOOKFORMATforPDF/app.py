bea2ff736fe73b84973a9ac0e59881fa94276658
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-01-23 16:22:45 +0900

    Update app.py

--- PDFtoBOOKFORMATforPDF/app.py
+++ PDFtoBOOKFORMATforPDF/app.py
@@ -75,7 +75,7 @@ for file in files:
             option = st.selectbox(
                 f'{file.name}の向きを指定: ',
                 ['左→右(横書き)', '右→左(縦書き)', 'aaa'],
-                index = idx
+                index = idx,
                 key = str(j)
             )
             if option == '左→右(横書き)':
