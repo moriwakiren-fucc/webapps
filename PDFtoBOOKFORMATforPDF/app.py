0aca6d43a99ae4c704ae534806c11e36a55c42e7
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-01-24 12:05:06 +0900

    Update app.py

--- PDFtoBOOKFORMATforPDF/app.py
+++ PDFtoBOOKFORMATforPDF/app.py
@@ -80,9 +80,8 @@ for j, file in enumerate(files):
             index = idx,
             key = 'muki' + str(j)
         )
-        name = st.text_input("ファイル名を入力",
+        name = st.text_input('ファイル名のうち、\'.pdf\'よりも前の部分を入力',
                              value=f'{file.name[:-4]}_BookFormat',
-                             place_holder='ファイル名のうち、\'.pdf\'よりも前の部分を入力'
                              key='name' + str(j))
         if option == '左→右(横書き)':
             muki = "LtoR"
