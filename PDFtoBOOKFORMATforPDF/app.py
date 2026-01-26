6284959760381ba145682e466879199d97008c62
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-01-26 15:16:19 +0900

    Update app.py

--- PDFtoBOOKFORMATforPDF/app.py
+++ PDFtoBOOKFORMATforPDF/app.py
@@ -8,7 +8,7 @@ st.title("PDF ページ毎→印刷用冊子形式")
 def pdfforPrint(org_pdf, muki, f_name, hyoushi=False, ura=False):
     if hyoushi or ura:
         with st.status("計算を開始します...", expanded=True) as status:
-            st.write()
+            st.write('HY')
             time.sleep(0.1)
         status.update(label="計算が完了しました ✅", state="complete")
     if muki != "RtoL" and muki != "LtoR":
