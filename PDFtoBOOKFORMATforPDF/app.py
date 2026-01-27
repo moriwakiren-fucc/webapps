ad41bfea4d4a3c45a9565caa8ee504cc42bb93b9
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-01-27 09:58:18 +0900

    Update app.py

--- PDFtoBOOKFORMATforPDF/app.py
+++ PDFtoBOOKFORMATforPDF/app.py
@@ -115,21 +115,21 @@ for j, file in enumerate(files):
         with st.status("処理中", expanded=False) as status:
             l = []
             time.sleep(3)
+                    if not l:
+                container.button(
+                    lavel = "処理中",
+                    disabled=True,
+                    key = 'predownload_' + str(j)
+                )
+            else:
+                container.download_button(
+                    label = "ダウンロード",
+                    data=l[0],
+                    file_name=l[1],
+                    mime='application/pdf',
+                    key = 'download' + str(j)
+                )
             l = pdfforPrint(file, muki, f_name, hyoushi, ura)
             time.sleep(0.2)
             status.update(label="処理が完了しました", state="complete")
-        if not l:
-            container.button(
-                lavel = "処理中",
-                disabled=True,
-                key = 'predownload_' + str(j)
-            )
-        else:
-            container.download_button(
-                label = "ダウンロード",
-                data=l[0],
-                file_name=l[1],
-                mime='application/pdf',
-                key = 'download' + str(j)
-            )
-    st.divider()
+        st.divider()
